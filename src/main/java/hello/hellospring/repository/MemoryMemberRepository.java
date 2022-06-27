ab468812025ae3b645948503b248eb5dee6403d4
Jeong-Daniel <empcik@pusan.ac.kr>
2022-06-27 23:16:17 +0900

    공부 추가

--- src/main/java/hello/hellospring/repository/MemoryMemberRepository.java
+++ src/main/java/hello/hellospring/repository/MemoryMemberRepository.java
@@ -22,12 +22,18 @@ public class MemoryMemberRepository implements MemberRepository {
     }
 
     @Override
-    public Optional<Member> findByName(String name){
-        return store.values().stream().filter(member -> member.getName().equals(name)).findAny();
+    public Optional<Member> findByName(String name) {
+        return store.values().stream()
+                .filter(member -> member.getName().equals(name))
+                .findAny();
     }
 
     @Override
     public List<Member> findAll() {
         return new ArrayList<>(store.values());
+    }
+
+    public void clearStore(){
+        store.clear();
     }
 }
--- src/test/java/hello/hellospring/repository/MemoryMemberRepositoryTest.java
+++ src/test/java/hello/hellospring/repository/MemoryMemberRepositoryTest.java
@@ -1,9 +1,21 @@
 package hello.hellospring.repository;
 
+import hello.hellospring.domain.Member;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Test;
 
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
 public class MemoryMemberRepositoryTest {
-    MemberRepository repository = new MemboryMemberRepository();
+    MemoryMemberRepository repository = new MemoryMemberRepository();
+
+    @AfterEach
+    public void afterEach(){
+        repository.clearStore();
+    }
 
     @Test
     public void save(){
@@ -15,17 +27,35 @@ public class MemoryMemberRepositoryTest {
         Member result = repository.findById(member.getId()).get();
         //System.out.println("result = " + (result == member));
         //Assertions.assertEquals(member, result)
-        Assertions.assertThat(member).isEqualTo(result);
+        assertThat(member).isEqualTo(result);
     }
 
     @Test
     public void findByName(){
         Member member1 = new Member();
-        member1.setName("srping1");
+        member1.setName("spring1");
         repository.save(member1);
 
         Member member2 = new Member();
-        member1.setName("srping2");
+        member1.setName("spring2");
         repository.save(member2);
+
+        Member result = repository.findByName("spring1").get();
+
+        assertThat(result).isEqualTo(member1);
+    }
+
+    @Test
+    public void findALL(){
+        Member member1 = new Member();
+        member1.setName("spring1");
+        repository.save(member1);
+
+        Member member2 = new Member();
+        member1.setName("spring2");
+        repository.save(member2);
+
+        List<Member> result = repository.findAll();
+        assertThat(result.size()).isEqualTo(2);
     }
 }
