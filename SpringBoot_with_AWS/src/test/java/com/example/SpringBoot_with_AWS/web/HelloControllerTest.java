fddc020cf8b01af240fc88dde41a300de959d632
Jeong-Daniel <empcik@pusan.ac.kr>
2022-07-29 20:03:04 +0900

    스프링부트 공부 업데이트

---
+++ SpringBoot_with_AWS/src/test/java/com/example/SpringBoot_with_AWS/web/HelloControllerTest.java
@@ -0,0 +1,36 @@
+package com.example.SpringBoot_with_AWS.web;
+
+//import org.junit.Test; junit4 구버전 대응
+import org.junit.jupiter.api.Test;
+//import org.junit.runner.RunWith; junit4 구버전 대응
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.FilterType;
+import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.hamcrest.Matchers.is;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@ExtendWith(SpringExtension.class)
+@WebMvcTest
+public class HelloControllerTest {
+    @Autowired
+    private MockMvc mvc;
+
+    @Test
+    public void hello가_리턴된다() throws Exception {
+        String hello = "hello";
+
+        mvc.perform(get("/hello"))
+                .andExpect(status().isOk())
+                .andExpect(content().string(hello));
+    }
+}
