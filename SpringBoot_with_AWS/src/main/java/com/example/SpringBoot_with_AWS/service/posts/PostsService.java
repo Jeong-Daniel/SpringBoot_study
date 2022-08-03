d9a9d495c29398fd1c4278792c6feff7b6f4d3e2
Jeong-Daniel <empcik@pusan.ac.kr>
2022-08-03 18:35:00 +0900

    150페이지 까지

--- SpringBoot_with_AWS/src/main/java/com/example/SpringBoot_with_AWS/service/posts/PostsService.java
+++ SpringBoot_with_AWS/src/main/java/com/example/SpringBoot_with_AWS/service/posts/PostsService.java
@@ -2,6 +2,7 @@ package com.example.SpringBoot_with_AWS.service.posts;
 
 import com.example.SpringBoot_with_AWS.domain.posts.Posts;
 import com.example.SpringBoot_with_AWS.domain.posts.PostsRepository;
+import com.example.SpringBoot_with_AWS.web.dto.PostsListResponseDto;
 import com.example.SpringBoot_with_AWS.web.dto.PostsSaveRequestDto;
 import com.example.SpringBoot_with_AWS.web.dto.PostsUpdateRequestDto;
 import com.example.SpringBoot_with_AWS.web.dto.PostsResponseDto;
@@ -38,5 +39,12 @@ public class PostsService {
         new IllegalArgumentException("해당 사용자가 없습니다. id=" + id));
 
         return new PostsResponseDto(entity);
+    }
+
+    @Transactional(readOnly = true)
+    public List<PostsListResponseDto> findAllDesc(){
+        return postsRepository.findAllDesc().stream()
+                .map(PostsListResponseDto::new)
+                .collect(Collectors.toList());
     }
 }
--- SpringBoot_with_AWS/src/main/java/com/example/SpringBoot_with_AWS/web/IndexController.java
+++ SpringBoot_with_AWS/src/main/java/com/example/SpringBoot_with_AWS/web/IndexController.java
@@ -1,13 +1,21 @@
 package com.example.SpringBoot_with_AWS.web;
 
+import com.example.SpringBoot_with_AWS.service.posts.PostsService;
+
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import org.springframework.ui.Model;
 
+@RequiredArgsConstructor
 @Controller
 public class IndexController {
+
+    private final PostsService postsService;
     @GetMapping("/")
-    public String index(){
+    public String index(Model model){
+        model.addAttribute("posts", postsService.findAllDesc());
         return "index";
     }
 
